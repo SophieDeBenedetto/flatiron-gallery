e01eca361cb684dfca1761377c11935032dd0531
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2016-06-21 15:43:02 -0400

    nest project routes under cohort

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :collaborations
-  resources :projects
+  # resources :collaborations
   resources :users
-  resources :cohorts
+  resources :cohorts do 
+    resources :projects
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
