9b2f99788f51c43a84b4d6d2f00ae84aa3c0aed3
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2016-06-29 13:29:50 -0400

    add fixtures for seeding to public dir, updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 require 'csv'
 cohort = Cohort.create(name: "web-0416")
-CSV.foreach('/Users/sophiedebenedetto/Downloads/web-0415-new.csv', headers: true) do |row|
+CSV.foreach(Rails.root.join('public', 'web-0415-new.csv'), headers: true) do |row|
   u = User.find_or_create_by(first_name: row["first_name"], last_name: row["last_name"], email: row["email"], github: row["github_username"], cohort: cohort)
   puts "created #{u.first_name} #{u.last_name}"
 end
 
-CSV.foreach('/Users/sophiedebenedetto/Desktop/bangarang-projects.csv', headers: true) do |row|
+CSV.foreach(Rails.root.join('public', 'bangarang-projects.csv'), headers: true) do |row|
   p = Project.find_or_create_by(deployed_url: row["Heroku/Surge Url"])
   p.github_repos = row["github"].split("," || "\n").map {|repo| repo.strip}
   p.name = row["Project Name"]
