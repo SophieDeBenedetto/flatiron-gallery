28bd39428eb5637121bad96123a067693a3febb3
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2016-06-30 19:14:45 -0400

    fix create.js.erb partial to render new_form; placeholder text on links fields

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -3,4 +3,28 @@ class UserDecorator < SimpleDelegator
   def show_avatar
     avatar.url == "/images/original/missing.png" ? "default-profile.png" : avatar.url(:medium)
   end
+
+  def twitter_form_field
+    self.twitter || "twitter"
+  end
+
+  def linkedin_form_field
+    self.linkedin || "linkedin"
+  end
+
+  def github_form_field
+    self.github || "github"
+  end
+
+  def personal_site_form_field
+    self.personal_site || "personal_site"
+  end
+
+  def blog_form_field
+    self.blog_url || "blog"
+  end
+
+  def email_form_field
+    self.email || "email"
+  end
 end
