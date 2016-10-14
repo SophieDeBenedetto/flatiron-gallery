fbe0e0d0e981d1c5adf83cd07845ccb82befc36f
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2016-10-14 14:27:40 -0400

    set up s3 region and host for prod env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,9 @@ Rails.application.configure do
   :s3_credentials => {
     :bucket => ENV['S3_BUCKET_NAME'],
     :access_key_id => ENV['AWS_KEY'],
-    :secret_access_key => ENV['AWS_SECRET']
+    :secret_access_key => ENV['AWS_SECRET'],
+    :s3_region => ENV['S3_REGION'],
+    :s3_host_name => ENV['S3_HOSTNAME']
   },
   :url =>':s3_domain_url',
   :path => "/:class/:attachment/:id_partition/:style/:filename" 
